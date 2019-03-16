1df4d6dde3f8178cdad93aa02d43212513a47425
siimav <siim.aaver@gmail.com>
2019-03-16 19:32:11 +0200

    Slightly changed how assembly versions are determined; Re-added dll to GameData;

--- Kerbal_Construction_Time/AssemblyVersion-RO.cs
+++ Kerbal_Construction_Time/AssemblyVersion-RO.cs
@@ -3,8 +3,6 @@
  // the next time this code is regenerated.
  // 
   
- #if KSP1_4
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.4.0.0")]
- #endif
+ [assembly: AssemblyFileVersion("1.4.5.0")]
